70592ca9817d7a20843acd9999fb9254ad321f96
ElW0ki <oscar5809@hotmail.com>
2024-10-02 17:33:20 -0600

    Initial commit

---
+++ Tarea1_parte2/cerradura/src/main/kotlin/com/ejercicio/cerradura/CerraduraApplication.kt
@@ -0,0 +1,11 @@
+package com.ejercicio.cerradura
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CerraduraApplication
+
+fun main(args: Array<String>) {
+	runApplication<CerraduraApplication>(*args)
+}
---
+++ Tarea1_parte2/cerradura/src/test/kotlin/com/ejercicio/cerradura/CerraduraApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ejercicio.cerradura
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CerraduraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
